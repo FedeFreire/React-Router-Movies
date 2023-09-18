d10b7ec290b6612d94f589012d1bdf3798c94eb4
FedeFreire <bandoleir@gmail.com>
2023-09-18 18:52:56 -0400

    Done, no stretch goals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+} from 'react-router-dom'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +24,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
+          console.log(response.data);
+          setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +42,12 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Routes>
+        <Route path="/" element={<MovieList movies={movies} />}>  
+        </Route>
+        <Route path="/movies/:id" element={<Movie movies={movies} />}>
+        </Route>
+      </Routes>
     </div>
   );
 }
